dfdf0d27ec08c0a9e7cb68393e564aef15ffc734
Chris92 <christopher.fluegel@gmail.com>
2021-08-19 12:16:37 +0000

    Added Tests

---
+++ EvoSC/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EvoSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, EvoSC!");
+        }
+    }
+}
