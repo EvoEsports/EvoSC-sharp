aaa7f777549e2e4c81652fec9c5909c994cfa2c3
snixtho <snixtho@gmail.com>
2021-08-19 13:51:12 +0200

    Fix project.

---
+++ EvoSC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvoSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
