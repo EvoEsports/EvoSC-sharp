ecd7b71cd7b4c8c26ed74dddda15a01e501f32a1
Chris-Lennart Junge <junge.cl@protonmail.com>
2022-02-13 11:10:46 +0100

    Added extension method for removing services from the service collection

---
+++ EvoSC/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class Extensions
+{
+    public static IServiceCollection Remove<T>(this IServiceCollection serviceCollection)
+    {
+        var serviceDescriptor = serviceCollection.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(T));
+        if (serviceDescriptor != null)
+        {
+            serviceCollection.Remove(serviceDescriptor);
+        }
+
+        return serviceCollection;
+    }
+}
