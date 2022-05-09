7bb27756903117a49036dda4a45ebe0221ed43bb
Christopher F <Chris92de@users.noreply.github.com>
2022-05-09 21:39:28 +0200

    Add Snippets.cs that I forgot earlier

---
+++ EvoSC/Core/Helpers/Snippets.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace EvoSC.Core.Helpers
+{
+    public class Snippets
+    {
+        public static string GetRelativeTimeToNow(DateTime dt)
+        {
+            const int SECOND = 1;
+            const int MINUTE = 60 * SECOND;
+            const int HOUR = 60 * MINUTE;
+            const int DAY = 24 * HOUR;
+            const int MONTH = 30 * DAY;
+
+            var ts = new TimeSpan(DateTime.UtcNow.Ticks - dt.Ticks);
+            double delta = Math.Abs(ts.TotalSeconds);
+
+            if (delta < 1 * MINUTE)
+                return ts.Seconds == 1 ? "one second ago" : ts.Seconds + " seconds ago";
+
+            if (delta < 2 * MINUTE)
+                return "a minute ago";
+
+            if (delta < 45 * MINUTE)
+                return ts.Minutes + " minutes ago";
+
+            if (delta < 90 * MINUTE)
+                return "an hour ago";
+
+            if (delta < 24 * HOUR)
+                return ts.Hours + " hours ago";
+
+            if (delta < 48 * HOUR)
+                return "yesterday";
+
+            if (delta < 30 * DAY)
+                return ts.Days + " days ago";
+
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "one month ago" : months + " months ago";
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+                return years <= 1 ? "one year ago" : years + " years ago";
+            }
+        }
+
+        public static string GetTimeSpentUntilNow(DateTime dt)
+        {
+            const int SECOND = 1;
+            const int MINUTE = 60 * SECOND;
+            const int HOUR = 60 * MINUTE;
+            const int DAY = 24 * HOUR;
+            const int MONTH = 30 * DAY;
+
+            var ts = new TimeSpan(DateTime.UtcNow.Ticks - dt.Ticks);
+            double delta = Math.Abs(ts.TotalSeconds);
+
+            if (delta < 1 * MINUTE)
+                return ts.Seconds == 1 ? "one second" : ts.Seconds + " seconds";
+
+            if (delta < 2 * MINUTE)
+                return "1 minute, " + (ts.Seconds - 60) + " seconds";
+
+            if (delta < 60 * MINUTE)
+                return ts.Minutes + " minutes";
+
+            if (delta < 120 * MINUTE)
+                return "1 hour, " + (ts.Minutes - 60) + " minutes";
+
+            if (delta < 24 * HOUR)
+                return ts.Hours + " hours";
+
+            if (delta < 48 * HOUR)
+                return "over a day";
+
+            if (delta < 30 * DAY)
+                return ts.Days + " days";
+
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "less than a month" : months + " months";
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+                return years <= 1 ? " year" : years + " years";
+            }
+        }
+    }
+}
