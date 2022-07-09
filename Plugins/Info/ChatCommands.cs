4d159edc0b06e8763af502b24ee0b591faa14d20
snixtho <snixtho@gmail.com>
2022-07-09 17:55:01 +0200

    Add info plugin to show version and help.

---
+++ Plugins/Info/ChatCommands.cs
@@ -0,0 +1,31 @@
+﻿using EvoSC.Core.Commands.Chat;
+using EvoSC.Core.Commands.Generic.Attributes;
+using EvoSC.Core.Helpers;
+using Newtonsoft.Json.Serialization;
+
+namespace Info;
+
+public class ChatCommands : ChatCommandGroup
+{
+    [Command("version", "Controller version.")]
+    public Task Version()
+    {
+        var evoscAssembly = AppDomain.CurrentDomain.GetAssemblies()
+            .First(a => a.GetName().Name == "EvoSC");
+
+        var name = evoscAssembly.GetName();
+
+        var message = new ChatMessage();
+        message.SetIcon(Icon.Information);
+        message.SetMessage($"{name.Name} v{name.Version}");
+        message.SetInfo();
+
+        return Context.Message.ReplyAsync(message.Render());
+    }
+
+    [Command("help", "Show information and help on controller usage.")]
+    public Task Help()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Plugins/Info/InfoPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.Design;
+using EvoSC.Core.Plugins;
+using EvoSC.Interfaces.Commands;
+using EvoSC.Interfaces.Players;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Info;
+
+public class InfoPlugin : IPlugin
+{
+    public string Name => "Info";
+    public Version Version => new(1, 0, 0);
+    
+    public void HandleEvents(IPlayerCallbacks playerCallbacks)
+    {
+    }
+
+    public void Register(IServiceCollection services)
+    {
+    }
+
+    public void Execute()
+    {
+    }
+
+    public void Unregister(IServiceCollection services)
+    {
+    }
+
+    public Task Load(IChatCommandsService commands)
+    {
+        commands.RegisterCommands<ChatCommands>();
+        return Task.CompletedTask;
+    }
+
+    public Task Unload(IChatCommandsService commands)
+    {
+        commands.UnregisterCommands<ChatCommands>();
+        return Task.CompletedTask;
+    }
+}
