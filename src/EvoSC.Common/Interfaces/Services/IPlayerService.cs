83522162bc72b9c53134fe1d13ab669cb38dab67
snixtho <snixtho@gmail.com>
2022-10-15 14:20:23 +0200

    add player service

--- src/EvoSC.Common/Interfaces/Services/IPlayerService.cs
+++ src/EvoSC.Common/Interfaces/Services/IPlayerService.cs
@@ -1,6 +1,39 @@
-﻿namespace EvoSC.Common.Interfaces.Services;
+﻿using EvoSC.Common.Database.Models;
 
-public class IPlayerService
+namespace EvoSC.Common.Interfaces.Services;
+
+public interface IPlayerService
 {
-    
+    /// <summary>
+    /// Get a player by it's database id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public Task<DbPlayer> GetPlayerById(int id);
+    /// <summary>
+    /// Get a player by it's login.
+    /// </summary>
+    /// <param name="login"></param>
+    /// <returns></returns>
+    public Task<DbPlayer> GetPlayerByLogin(string login);
+    /// <summary>
+    /// Create a new player in the database and return the new player object.
+    /// </summary>
+    /// <param name="login"></param>
+    /// <param name="ubisoftName"></param>
+    /// <param name="zone"></param>
+    /// <returns></returns>
+    public Task<DbPlayer> NewPlayer(string login, string ubisoftName, string? zone=null);
+    /// <summary>
+    /// Update a player's database entry.
+    /// </summary>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    public Task<bool> UpdatePlayer(DbPlayer player);
+    /// <summary>
+    /// Delete a player from the database.
+    /// </summary>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    public Task<bool> DeletePlayer(DbPlayer player);
 }
--- src/EvoSC.Common/Services/PlayerService.cs
+++ src/EvoSC.Common/Services/PlayerService.cs
@@ -1,11 +1,51 @@
-﻿using EvoSC.Common.Interfaces.Services;
+﻿using System.Data.Common;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using EvoSC.Common.Database.Models;
+using EvoSC.Common.Interfaces.Services;
+using Microsoft.Extensions.Logging;
 
 namespace EvoSC.Common.Services;
 
 public class PlayerService : IPlayerService
 {
-    public PlayerService()
+    private readonly ILogger<PlayerService> _logger;
+    private readonly DbConnection _db;
+
+    public PlayerService(ILogger<PlayerService> logger, DbConnection db)
     {
-        
+        _logger = logger;
+        _db = db;
     }
+
+    public Task<DbPlayer> GetPlayerById(int id) =>
+        _db.GetAsync<DbPlayer>(id);
+
+    public async Task<DbPlayer> GetPlayerByLogin(string login) =>
+        (await _db.QueryAsync("select * from players where Login=@Login limit 1", new {Login = login}))
+        .FirstOrDefault();
+
+    public async Task<DbPlayer> NewPlayer(string login, string ubisoftName, string? zone = null)
+    {
+        var newPlayer = new DbPlayer
+        {
+            Login = login,
+            UbisoftName = ubisoftName,
+            Zone = zone,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+
+        var id = await _db.InsertAsync(newPlayer);
+        return await GetPlayerById(id);
+    }
+
+    public Task<bool> UpdatePlayer(DbPlayer player)
+    {
+        player.UpdatedAt = DateTime.UtcNow;
+        return _db.UpdateAsync(player);
+    }
+
+    public Task<bool> DeletePlayer(DbPlayer player) =>
+        _db.DeleteAsync(player);
 }
