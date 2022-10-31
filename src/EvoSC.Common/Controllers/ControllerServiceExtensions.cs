24ef51022019da360189235bc76f971c6c4faf6c
snixtho <snixtho@gmail.com>
2022-10-31 11:33:05 +0100

    Remove unused code.

--- src/EvoSC.Common/Controllers/ControllerServiceExtensions.cs
+++ src/EvoSC.Common/Controllers/ControllerServiceExtensions.cs
@@ -9,7 +9,6 @@ public static class ControllerServiceExtensions
 {
     public static Container AddEvoScControllers(this Container services)
     {
-        // return services.AddSingleton<IControllerManager, ControllerManager>();
         services.RegisterSingleton<IControllerManager, ControllerManager>();
         return services;
     }
