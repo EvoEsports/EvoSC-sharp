bb3b11997c7e5239fe40b7fb698447a89d244c6d
snixtho <snixtho@users.noreply.github.com>
2022-12-16 13:37:36 +0100

    Support modescript callbacks (#86)

---
+++ src/EvoSC.Common/Remote/EventArgsModels/ModeScriptEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace EvoSC.Common.Remote.EventArgsModels;
+
+public class ModeScriptEventArgs : EventArgs
+{
+    public required string Method { get; init; }
+    public required JObject Args { get; init; }
+}
---
+++ src/EvoSC.Common/Remote/EventArgsModels/PlayerUpdateEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace EvoSC.Common.Remote.EventArgsModels;
+
+public class PlayerUpdateEventArgs : EventArgs
+{
+    public required int Time { get; init; }
+    public required string Login { get; init; }
+    public required string AccountId { get; init; }
+}
