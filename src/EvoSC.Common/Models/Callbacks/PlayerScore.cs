55f84f1cd9a3faa705af8e2e95be622456276754
snixtho <snixtho@users.noreply.github.com>
2022-12-16 13:37:36 +0100

    Support modescript callbacks (#86)

---
+++ src/EvoSC.Common/Models/Callbacks/PlayerScore.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace EvoSC.Common.Models.Callbacks;
+
+public class PlayerScore
+{
+    [JsonProperty("login")]
+    public string Login { get; init; }
+    
+    [JsonProperty("accountid")]
+    public string AccountId { get; init; }
+    
+    [JsonProperty("name")]
+    public string Name { get; init; }
+    
+    [JsonProperty("rank")]
+    public int Rank { get; init; }
+    
+    [JsonProperty("roundpoints")]
+    public int RoundPoints { get; init; }
+    
+    [JsonProperty("mappoints")]
+    public int MapPoints { get; init; }
+    
+    [JsonProperty("matchpoints")]
+    public int MatchPoints { get; init; }
+    
+    [JsonProperty("bestracetime")]
+    public int BestRaceTime { get; init; }
+    
+    [JsonProperty("bestracecheckpoints")]
+    public IEnumerable<int> BestRaceCheckpoints { get; init; }
+    
+    [JsonProperty("bestlaptime")]
+    public int BestLapTime { get; init; }
+    
+    [JsonProperty("bestlapcheckpoints")]
+    public IEnumerable<int> BestLapCheckpoints { get; init; }
+    
+    [JsonProperty("prevracetime")]
+    public int PreviousRaceTime { get; init; }
+    
+    [JsonProperty("prevracecheckpoints")]
+    public IEnumerable<int> PreviousRaceCheckpoints { get; init; }
+}
---
+++ src/EvoSC.Common/Remote/EventArgsModels/ScoresEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using EvoSC.Common.Models.Callbacks;
+
+namespace EvoSC.Common.Remote.EventArgsModels;
+
+public class ScoresEventArgs : EventArgs
+{
+    public required string? Section { get; init; }
+    public required bool UseTeams { get; init; }
+    public required int WinnerTeam { get; init; }
+    public required string? WinnerPlayer { get; init; }
+    public required IEnumerable<TeamScore?> Teams { get; init; }
+    public required IEnumerable<PlayerScore?> Players { get; init; }
+}
