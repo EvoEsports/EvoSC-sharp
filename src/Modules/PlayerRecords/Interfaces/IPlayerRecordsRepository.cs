4afec9cfad9ce003c51244c23beda4498e6073b6
snixtho <snixtho@gmail.com>
2022-12-29 14:56:09 +0100

    add documentation

--- src/Modules/PlayerRecords/Interfaces/IPlayerRecordsRepository.cs
+++ src/Modules/PlayerRecords/Interfaces/IPlayerRecordsRepository.cs
@@ -5,7 +5,25 @@ namespace EvoSC.Modules.Official.PlayerRecords.Interfaces;
 
 public interface IPlayerRecordsRepository
 {
+    /// <summary>
+    /// Get a record from a player in a specific map.
+    /// </summary>
+    /// <param name="player">The player that has the record.</param>
+    /// <param name="map">The map to get the record from.</param>
+    /// <returns></returns>
     public Task<DbPlayerRecord?> GetRecordAsync(IPlayer player, IMap map);
+    
+    /// <summary>
+    /// Update a record in the database.
+    /// </summary>
+    /// <param name="record">The record to update with new information.</param>
+    /// <returns></returns>
     public Task UpdateRecordAsync(DbPlayerRecord record);
+    
+    /// <summary>
+    /// Add a new record to the database.
+    /// </summary>
+    /// <param name="record">The record to add.</param>
+    /// <returns></returns>
     public Task InsertRecordAsync(DbPlayerRecord record);
 }
