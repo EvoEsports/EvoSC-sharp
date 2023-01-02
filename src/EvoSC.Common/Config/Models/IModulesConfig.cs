ed860a107e3916af782a3e11d7ec4bdd85111e87
stefaneeck <stefan.eeckhoudt@gmail.com>
2023-01-02 20:28:08 +0100

    GH-89: Fixing default config.

---
+++ src/EvoSC.Common/Config/Models/IModulesConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Config.Net;
+
+namespace EvoSC.Common.Config.Models;
+
+public interface IModulesConfig
+{
+    [Description("Signature verification of module's files. If enabled and verification fails, the module will not load.")]
+    [Option(Alias = "requireSignatureVerification ", DefaultValue = "true")]
+    public string RequireSignatureVerification { get; }
+
+    [Description("Directories to scan for external modules.")]
+    [Option(Alias = "moduleDirectories ", DefaultValue = "[ \"modules\" ]")]
+    public string ModuleDirectories { get; }
+
+}
--- src/EvoSC.Common/Config/Stores/TomlConfigStore.cs
+++ src/EvoSC.Common/Config/Stores/TomlConfigStore.cs
@@ -1,8 +1,8 @@
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.IO;
 using System.Reflection;
 using Config.Net;
 using EvoSC.Common.Util;
-using EvoSC.Common.Util.TextFormatting;
 using Tomlet;
 using Tomlet.Models;
 
@@ -23,7 +23,7 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
             {
                 Directory.CreateDirectory(directory);
             }
-
+            
             _document = CreateDefaultConfig();
             File.WriteAllText(path, _document.SerializedValue);
 
@@ -32,6 +32,7 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
         {
             _document = TomlParser.ParseFile(path);
         }
+        
         _path = Path.GetFullPath(path);
     }
 
@@ -42,10 +43,10 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
 
         // avoid inline writing which is more human readable
         document.ForceNoInline = false;
-
+        
         return document;
     }
-
+    
     private TomlDocument BuildSubDocument(TomlDocument document, Type type, string name)
     {
         foreach (var property in type.GetProperties())
@@ -62,26 +63,22 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
                 // get property name
                 var propName = optionAttr?.Alias ?? property.Name;
                 propName = name == "" ? propName : $"{name}.{propName}";
-
-                var tomlValue = optionAttr?.DefaultValue ?? property.PropertyType.GetDefaultTypeValue();
-                if (property.PropertyType == typeof(TextColor))
-                    tomlValue = new TextColor(tomlValue.ToString());
-
+                
                 // get property value
                 var value = TomletMain.ValueFrom(property.PropertyType,
-                    tomlValue);
+                    optionAttr?.DefaultValue ?? property.PropertyType.GetDefaultTypeValue());
 
                 // add description/comment if defined
                 if (descAttr != null)
                 {
                     value.Comments.PrecedingComment = descAttr.Description;
                 }
-
+                
                 // write to document
                 document.Put(propName, value);
             }
         }
-
+        
         return document;
     }
 
@@ -90,36 +87,14 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
         // do nothing because the document lives for the entire application and is disposed on shutdown
     }
 
-    private string GetArrayValue(string key)
-    {
-        var value = _document.GetValue(key) as TomlArray;
-
-        return string.Join(" ", value.Select(v => v.StringValue));
-    }
     public string? Read(string key)
     {
-        var lastDotIndex = key.LastIndexOf(".", StringComparison.Ordinal);
-
-        if (lastDotIndex > 0 && key.Length > lastDotIndex + 1 && !char.IsAsciiLetterOrDigit(key[lastDotIndex + 1]))
-        {
-            var value = _document.GetValue(key[..lastDotIndex]) as TomlArray;
-            return value.Count.ToString();
-        }
-        else if (key.EndsWith("]", StringComparison.Ordinal))
-        {
-            var indexStart = key.IndexOf("[", StringComparison.Ordinal);
-            var index = int.Parse(key[(indexStart + 1)..^1]);
-            var value = _document.GetValue(key[..indexStart]) as TomlArray;
-
-            return value?.Skip(index)?.FirstOrDefault()?.StringValue;
-        }
-
         return _document.GetValue(key).StringValue;
     }
 
     public void Write(string key, string? value)
     {
-        throw new NotSupportedException();
+        throw new NotImplementedException();
     }
 
     public bool CanRead => true;
