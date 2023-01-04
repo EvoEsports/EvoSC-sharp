ee3e644751cdf32726899b03cfe0bd7beabd3822
snixtho <snixtho@gmail.com>
2023-01-04 17:00:40 +0100

    fix some PR issues

--- src/Modules/PlayerRecords/Database/Models/DbPlayerRecord.cs
+++ src/Modules/PlayerRecords/Database/Models/DbPlayerRecord.cs
@@ -9,15 +9,16 @@ public class DbPlayerRecord : IPlayerRecord
 {
     [Key]
     public long Id { get; set; }
-    [Computed]
-    public IPlayer Player { get; set; }
     public long PlayerId { get; set; }
-    [Computed]
-    public IMap? Map { get; set; }
     public long? MapId { get; set; }
     public int Score { get; set; }
     public PlayerRecordType RecordType { get; set; }
     public string? Checkpoints { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+    
+    [Computed]
+    public IPlayer Player { get; set; }
+    [Computed]
+    public IMap? Map { get; set; }
 }
