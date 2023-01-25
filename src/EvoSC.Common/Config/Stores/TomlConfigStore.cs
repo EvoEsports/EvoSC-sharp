f4c0472e1a8000568361ad02051bd12d74d59c99
snixtho <snixtho@gmail.com>
2023-01-25 09:30:17 +0100

    Create a modular system for TOML type mappers and fix array not being generated.

--- src/EvoSC.Common/Config/Stores/TomlConfigStore.cs
+++ src/EvoSC.Common/Config/Stores/TomlConfigStore.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
 using Config.Net;
 using EvoSC.Common.Util;
+using EvoSC.Common.Util.TextFormatting;
 using Tomlet;
+using Tomlet.Exceptions;
 using Tomlet.Models;
 
 namespace EvoSC.Common.Config.Stores;
@@ -23,7 +26,7 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
             {
                 Directory.CreateDirectory(directory);
             }
-            
+
             _document = CreateDefaultConfig();
             File.WriteAllText(path, _document.SerializedValue);
 
@@ -32,7 +35,6 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
         {
             _document = TomlParser.ParseFile(path);
         }
-        
         _path = Path.GetFullPath(path);
     }
 
@@ -43,13 +45,14 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
 
         // avoid inline writing which is more human readable
         document.ForceNoInline = false;
-        
+
         return document;
     }
-    
+
     private TomlDocument BuildSubDocument(TomlDocument document, Type type, string name)
     {
-        foreach (var property in type.GetProperties())
+        var properties = type.GetProperties();
+        foreach (var property in properties)
         {
             if (property.PropertyType.IsInterface)
             {
@@ -64,21 +67,27 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
                 var propName = optionAttr?.Alias ?? property.Name;
                 propName = name == "" ? propName : $"{name}.{propName}";
                 
+                Console.WriteLine(propName);
+
+                var tomlValue = optionAttr?.DefaultValue ?? property.PropertyType.GetDefaultTypeValue();
+                if (property.PropertyType == typeof(TextColor))
+                    tomlValue = new TextColor(tomlValue.ToString());
+
                 // get property value
                 var value = TomletMain.ValueFrom(property.PropertyType,
-                    optionAttr?.DefaultValue ?? property.PropertyType.GetDefaultTypeValue());
+                    tomlValue);
 
                 // add description/comment if defined
                 if (descAttr != null)
                 {
                     value.Comments.PrecedingComment = descAttr.Description;
                 }
-                
+
                 // write to document
                 document.Put(propName, value);
             }
         }
-        
+
         return document;
     }
 
@@ -87,14 +96,36 @@ public class TomlConfigStore<TConfig> : IConfigStore where TConfig : class
         // do nothing because the document lives for the entire application and is disposed on shutdown
     }
 
+    private string GetArrayValue(string key)
+    {
+        var value = _document.GetValue(key) as TomlArray;
+
+        return string.Join(" ", value.Select(v => v.StringValue));
+    }
     public string? Read(string key)
     {
+        var lastDotIndex = key.LastIndexOf(".", StringComparison.Ordinal);
+
+        if (lastDotIndex > 0 && key.Length > lastDotIndex + 1 && !char.IsAsciiLetterOrDigit(key[lastDotIndex + 1]))
+        {
+            var value = _document.GetValue(key[..lastDotIndex]) as TomlArray;
+            return value.Count.ToString();
+        }
+        else if (key.EndsWith("]", StringComparison.Ordinal))
+        {
+            var indexStart = key.IndexOf("[", StringComparison.Ordinal);
+            var index = int.Parse(key[(indexStart + 1)..^1]);
+            var value = _document.GetValue(key[..indexStart]) as TomlArray;
+
+            return value?.Skip(index)?.FirstOrDefault()?.StringValue;
+        }
+
         return _document.GetValue(key).StringValue;
     }
 
     public void Write(string key, string? value)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 
     public bool CanRead => true;
