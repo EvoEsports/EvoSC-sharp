b656258e87d6c0401ee00e36734c66db09141815
snixtho <snixtho@gmail.com>
2023-01-25 09:32:45 +0100

    Add doc comments.

--- src/EvoSC.Common/Interfaces/Config/Mapping/ITomlMappingManager.cs
+++ src/EvoSC.Common/Interfaces/Config/Mapping/ITomlMappingManager.cs
@@ -2,7 +2,15 @@
 
 public interface ITomlMappingManager
 {
+    /// <summary>
+    /// Add a new type mapper.
+    /// </summary>
+    /// <param name="mapper">An instance of the type mapper to add.</param>
+    /// <typeparam name="T">The type that should be converted.</typeparam>
     public void AddMapper<T>(ITomlTypeMapper<T> mapper);
 
+    /// <summary>
+    /// Set up the default type mappers in EvoSC.
+    /// </summary>
     public void SetupDefaultMappers();
 }
