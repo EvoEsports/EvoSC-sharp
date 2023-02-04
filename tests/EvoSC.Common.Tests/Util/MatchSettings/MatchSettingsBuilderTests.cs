c9de8596e7ee18ffd9f65569f68945283e6590fa
snixtho <snixtho@gmail.com>
2023-02-04 17:06:36 +0100

    add single test

---
+++ tests/EvoSC.Common.Tests/Util/MatchSettings/MatchSettingsBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿using EvoSC.Common.Util.MatchSettings;
+using EvoSC.Common.Util.MatchSettings.Builders;
+using Xunit;
+
+namespace EvoSC.Common.Tests.Util.MatchSettings;
+
+public class MatchSettingsBuilderTests
+{
+    [Theory]
+    [InlineData(DefaultModeScriptName.TimeAttack, "Trackmania/TM_TimeAttack_Online.Script.txt")]
+    public void Set_Default_Mode_Script_Name(DefaultModeScriptName name, string expected)
+    {
+        var builder = new MatchSettingsBuilder();
+        builder.WithMode(name);
+
+        var ms = builder.Build();
+        
+        Assert.Equal(expected, ms.GameInfos.ScriptName);
+    }
+}
