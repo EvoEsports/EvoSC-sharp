d0dce4c177448634da31c77fff9070908a3f6b3d
snixtho <snixtho@gmail.com>
2023-02-07 10:00:56 +0100

    add exception comments to interface

--- src/EvoSC.Common/Interfaces/Services/IMatchSettingsService.cs
+++ src/EvoSC.Common/Interfaces/Services/IMatchSettingsService.cs
@@ -1,5 +1,6 @@
 ﻿using EvoSC.Common.Interfaces.Util;
 using EvoSC.Common.Util.MatchSettings.Builders;
+using GbxRemoteNet.Exceptions;
 
 namespace EvoSC.Common.Interfaces.Services;
 
@@ -9,6 +10,7 @@ public interface IMatchSettingsService
     /// Set the current ModeScript settings.
     /// </summary>
     /// <param name="settingsAction">Fluent action for modifying script settings.</param>
+    /// <exception cref="InvalidOperationException">Thrown when the settings obtained from the server is null.</exception>
     /// <returns></returns>
     public Task SetCurrentScriptSettingsAsync(Action<Dictionary<string, object>> settingsAction);
 
@@ -16,6 +18,7 @@ public interface IMatchSettingsService
     /// Set the current ModeScript settings from a match settings info object.
     /// </summary>
     /// <param name="matchSettings">The match settings containing the mode settings to set.</param>
+    /// <exception cref="InvalidOperationException">Thrown when the settings obtained from the server is null.</exception>
     /// <returns></returns>
     public Task SetCurrentScriptSettingsAsync(IMatchSettings matchSettings);
     
@@ -29,6 +32,8 @@ public interface IMatchSettingsService
     /// Load a MatchSettings file.
     /// </summary>
     /// <param name="name">The name of the MatchSettings to load.</param>
+    /// <exception cref="FileNotFoundException">Thrown when the server failed to find the match settings file.</exception>
+    /// <exception cref="XmlRpcFaultException">Thrown if XMLRPC raised an unknown error.</exception>
     /// <returns></returns>
     public Task LoadMatchSettingsAsync(string name);
 
@@ -37,6 +42,7 @@ public interface IMatchSettingsService
     /// </summary>
     /// <param name="name">The name of the file. Do not include extension. Directory paths allowed. Relative to MatchSettings directory.</param>
     /// <param name="matchSettings">Match settings builder for creation.</param>
+    /// <exception cref="InvalidOperationException">Thrown when the server failed to save the match settings file.</exception>
     /// <returns></returns>
     public Task<IMatchSettings> CreateMatchSettingsAsync(string name, Action<MatchSettingsBuilder> matchSettings);
 
@@ -45,6 +51,8 @@ public interface IMatchSettingsService
     /// info about it.
     /// </summary>
     /// <param name="name">The name of the file. Do not include extension. Directory paths allowed. Relative to MatchSettings directory.</param>
+    /// <exception cref="DirectoryNotFoundException">Thrown if the default or custom (from config) maps directory does not exist.</exception>
+    /// <exception cref="FileNotFoundException">Thrown if the match settings file was not found.</exception>
     /// <returns></returns>
     public Task<IMatchSettings> GetMatchSettingsAsync(string name);
     
@@ -53,6 +61,7 @@ public interface IMatchSettingsService
     /// </summary>
     /// <param name="name">The name of the match settings file.</param>
     /// <param name="builderAction">Fluent builder for editing the contents.</param>
+    /// <exception cref="InvalidOperationException">Thrown when the server failed to save the match settings file.</exception>
     /// <returns></returns>
     public Task EditMatchSettingsAsync(string name, Action<MatchSettingsBuilder> builderAction);
     
@@ -60,6 +69,8 @@ public interface IMatchSettingsService
     /// Remove a match settings file.
     /// </summary>
     /// <param name="name">The name of the match settings file.</param>
+    /// <exception cref="DirectoryNotFoundException">Thrown if the default or custom (from config) maps directory does not exist.</exception>
+    /// <exception cref="FileNotFoundException">Thrown if the match settings file was not found.</exception>
     /// <returns></returns>
     public Task DeleteMatchSettingsAsync(string name);
 }
