841fdec535d961c36d61e1bf07dce8a964b4ff50
snixtho <snixtho@gmail.com>
2023-02-13 10:57:23 +0100

    add more comments

--- src/EvoSC.Common/Interfaces/Database/Repository/IAuditRepository.cs
+++ src/EvoSC.Common/Interfaces/Database/Repository/IAuditRepository.cs
@@ -5,5 +5,10 @@ namespace EvoSC.Common.Interfaces.Database.Repository;
 
 public interface IAuditRepository
 {
+    /// <summary>
+    /// Add a new audit record to the database.
+    /// </summary>
+    /// <param name="record">The audit to add.</param>
+    /// <returns></returns>
     public Task<IAuditRecord> AddRecordAsync(DbAuditRecord record);
 }
