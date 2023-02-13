b7cd36b8af46f2a02bb47131ed5ac7536b289781
snixtho <snixtho@gmail.com>
2023-02-13 11:14:38 +0100

    Allow tests to access internals.

---
+++ tests/EvoSC.Common.Tests/Util/AuditEventBuilderTests.cs
@@ -0,0 +1,18 @@
+﻿using EvoSC.Common.Util.Auditing;
+using Xunit;
+
+namespace EvoSC.Common.Tests.Util;
+
+public class AuditEventBuilderTests
+{
+    public AuditEventBuilderTests()
+    {
+        
+    }
+
+    [Fact]
+    public void No_Event_Name_Constructor_Starts_Has_Correct_Defaults()
+    {
+        var builder = new AuditEventBuilder(null);
+    }
+}
