0bbe413e4e78fed2a7e18bb9c36aa661c3152953
snixtho <snixtho@gmail.com>
2023-02-13 11:41:09 +0100

    add tests

--- src/EvoSC.Common/Database/Migrations/202302081052_AddAuditLogTable.cs
+++ src/EvoSC.Common/Database/Migrations/202302081052_AddAuditLogTable.cs
@@ -10,7 +10,7 @@ public class AddAuditLogTable : Migration
     public override void Up()
     {
         Create.Table(AuditLog)
-            .WithColumn("Id").AsInt64().Identity()
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
             .WithColumn("Status").AsInt16()
             .WithColumn("EventName").AsString().Indexed()
             .WithColumn("Comment").AsString()
