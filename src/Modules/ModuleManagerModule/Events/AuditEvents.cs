74dd39e47c6f79407125e2420bf14a2f2b48111c
snixtho <snixtho@gmail.com>
2023-06-16 10:30:14 +0200

    some more comments

--- src/Modules/ModuleManagerModule/Events/AuditEvents.cs
+++ src/Modules/ModuleManagerModule/Events/AuditEvents.cs
@@ -4,9 +4,15 @@ namespace EvoSC.Modules.Official.ModuleManagerModule.Events;
 
 public enum AuditEvents
 {
+    /// <summary>
+    /// Triggered when a module is enabled by a user.
+    /// </summary>
     [Identifier(Name = "ModuleManager:ModuleEnabled")]
     ModuleEnabled,
     
+    /// <summary>
+    /// Triggered when a module is disabled by a user.
+    /// </summary>
     [Identifier(Name = "ModuleManager:ModuleDisabled")]
     ModuleDisabled
 }
