ab66c40f1a08ae1630e4d1973b62989618d67762
snixtho <snixtho@gmail.com>
2023-07-17 10:52:49 +0200

    fix missing permissions for map adding/removing

---
+++ src/Modules/MapsModule/MapsPermissions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using EvoSC.Common.Permissions.Attributes;
+
+namespace EvoSC.Modules.Official.Maps;
+
+[PermissionGroup]
+public enum MapsPermissions
+{
+    [Description("Allow adding maps to the server.")]
+    AddMap,
+    
+    [Description("Allow removing maps from the server.")]
+    RemoveMap
+}
