80eefcc175097a2b20786be6b475c6202b1d24f2
snixtho <snixtho@gmail.com>
2023-08-02 13:22:15 +0200

    add controller tests

---
+++ tests/Modules/OpenPlanetModule.Tests/Controllers/OpenPlanetControlManialinkControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using EvoSC.Common.Interfaces;
+using EvoSC.Common.Interfaces.Models;
+using EvoSC.Manialinks.Interfaces.Models;
+using EvoSC.Modules.Official.OpenPlanetModule.Controllers;
+using EvoSC.Modules.Official.OpenPlanetModule.Interfaces;
+using EvoSC.Modules.Official.OpenPlanetModule.Interfaces.Models;
+using EvoSC.Testing;
+using EvoSC.Testing.Controllers;
+using GbxRemoteNet.Interfaces;
+using Moq;
+
+namespace OpenPlanetModule.Tests.Controllers;
+
+public class OpenPlanetControlManialinkControllerTests : ManialinkControllerTestBase<OpenPlanetControlManialinkController>
+{
+    private const string PlayerAccountId = "a467a996-eba5-44bf-9e2b-8543b50f39ae";
+    private const string PlayerLogin = "pGepluulRL-eK4VDtQ85rg";
+    private readonly Mock<IOnlinePlayer> _player = new();
+    private readonly Mock<IManialinkActionContext> _actionContext = new();
+    private readonly Mock<IOpenPlanetControlService> _controlService = new();
+    private readonly (Mock<IServerClient> Server, Mock<IGbxRemoteClient> Client) _server = Mocking.NewServerClientMock();
+    private readonly Mock<IOpenPlanetTrackerService> _trackerService = new();
+
+    public OpenPlanetControlManialinkControllerTests()
+    {
+        _player.Setup(p => p.AccountId).Returns(PlayerAccountId);
+        
+        InitMock(_player.Object, _actionContext.Object, _controlService, _server.Item1, _trackerService);
+    }
+
+    [Fact]
+    public async Task Player_Is_Verified_With_Correct_OP_Configuration()
+    {
+        var infoMock = new Mock<IOpenPlanetInfo>();
+        await Controller.CheckAsync(infoMock.Object);
+
+        _controlService.Verify(s => s.VerifySignatureModeAsync(_player.Object, infoMock.Object), Times.Once);
+        _trackerService.Verify(s => s.AddOrUpdatePlayer(_player.Object, infoMock.Object));
+    }
+
+    [Fact]
+    public async Task Player_Is_Kicked_On_Disconnect_Action()
+    {
+        await Controller.DisconnectAsync();
+        
+        _server.Client.Verify(c => c.KickAsync(PlayerLogin));
+    }
+}
---
+++ tests/Modules/OpenPlanetModule.Tests/Controllers/OpenPlanetEventControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using EvoSC.Common.Interfaces;
+using EvoSC.Common.Interfaces.Models;
+using EvoSC.Common.Interfaces.Services;
+using EvoSC.Modules.Official.OpenPlanetModule.Controllers;
+using EvoSC.Modules.Official.OpenPlanetModule.Interfaces;
+using EvoSC.Modules.Official.OpenPlanetModule.Models;
+using EvoSC.Testing;
+using EvoSC.Testing.Controllers;
+using GbxRemoteNet.Events;
+using GbxRemoteNet.Interfaces;
+using Moq;
+
+namespace OpenPlanetModule.Tests.Controllers;
+
+public class OpenPlanetEventControllerTests : EventControllerTestBase<OpenPlanetEventController>
+{
+    private const string PlayerAccountId = "a467a996-eba5-44bf-9e2b-8543b50f39ae";
+    private const string PlayerLogin = "pGepluulRL-eK4VDtQ85rg";
+    
+    private readonly Mock<IOpenPlanetTrackerService> _trackerService = new();
+    private readonly (Mock<IServerClient> Server, Mock<IGbxRemoteClient> Client) _server = Mocking.NewServerClientMock();
+    private readonly Mock<IPlayerManagerService> _playerService = new();
+    private Mock<IPlayer> _player = new();
+
+    public OpenPlanetEventControllerTests()
+    {
+        _player.Setup(p => p.AccountId).Returns(PlayerAccountId);
+        InitMock(_server.Server, _trackerService, _playerService);
+    }
+
+    [Fact]
+    public async Task Player_Due_For_Kick_Is_Kicked()
+    {
+        await Controller.OnPlayerDueForKickAsync(null, new PlayerDueForKickEventArgs {Player = _player.Object});
+
+        _server.Client.Verify(c => c.KickAsync(PlayerLogin), Times.Once);
+    }
+
+    [Fact]
+    public async Task Player_Disconnected_Is_Removed_From_Tracker()
+    {
+        _playerService
+            .Setup(s => s.GetPlayerAsync(PlayerLogin))
+            .Returns(Task.FromResult((IPlayer?)_player.Object));
+
+        await Controller.OnPlayerDisconnect(null,
+            new PlayerDisconnectGbxEventArgs {Login = PlayerLogin, Reason = null});
+        
+        _trackerService.Verify(s => s.RemovePlayer(_player.Object), Times.Once);
+    }
+}
