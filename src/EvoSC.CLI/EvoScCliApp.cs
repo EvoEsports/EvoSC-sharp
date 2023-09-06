0926b4831f61d4aff1dc96238bc9289272d2406f
snixtho <snixtho@gmail.com>
2023-09-06 13:10:56 +0200

    Config generator CLI command.

---
+++ src/EvoSC.CLI/EvoScCliApp.cs
@@ -0,0 +1,26 @@
+﻿using EvoSC.Common.Interfaces;
+using SimpleInjector;
+
+namespace EvoSC.CLI;
+
+public class EvoScCliApp : IEvoSCApplication
+{
+    public IStartupPipeline StartupPipeline { get; }
+    public CancellationToken MainCancellationToken { get; }
+    public Container Services { get; }
+
+    public EvoScCliApp()
+    {
+        
+    }
+    
+    public Task RunAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ShutdownAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
