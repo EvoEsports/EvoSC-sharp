732083c6244a7884d411e3f697f2e6a80c39939f
snixtho <snixtho@gmail.com>
2023-09-07 12:41:05 +0200

    Add some error messages if something goes wrong to players.

--- src/Modules/GeardownModule/Controllers/EventsController.cs
+++ src/Modules/GeardownModule/Controllers/EventsController.cs
@@ -1,6 +1,7 @@
 ﻿using EvoSC.Common.Controllers;
 using EvoSC.Common.Controllers.Attributes;
 using EvoSC.Common.Events.Attributes;
+using EvoSC.Common.Interfaces;
 using EvoSC.Common.Interfaces.Controllers;
 using EvoSC.Common.Remote;
 using EvoSC.Modules.Evo.GeardownModule.Interfaces;
@@ -19,11 +20,13 @@ public class EventsController : EvoScController<IEventControllerContext>
 {
     private readonly IGeardownService _geardownService;
     private readonly IGeardownSettings _settings;
+    private readonly IServerClient _server;
 
-    public EventsController(IGeardownService geardownService, IGeardownSettings settings)
+    public EventsController(IGeardownService geardownService, IGeardownSettings settings, IServerClient server)
     {
         _geardownService = geardownService;
         _settings = settings;
+        _server = _server;
     }
 
     [Subscribe(MatchReadyEvents.AllPlayersReady)]
@@ -33,8 +36,17 @@ public class EventsController : EvoScController<IEventControllerContext>
         {
             return;
         }
-        
-        await _geardownService.StartMatchAsync();
+
+        try
+        {
+            await _geardownService.StartMatchAsync();
+        }
+        catch (Exception)
+        {
+            await _server.ErrorMessageAsync(
+                "An error occured while trying to start the match. Contact match admin immediately.");
+            throw;
+        }
     }
 
     [Subscribe(MatchTrackerEvent.StateTracked)]
@@ -42,10 +54,30 @@ public class EventsController : EvoScController<IEventControllerContext>
     {
         if (args.State.Status == MatchStatus.Ended)
         {
-            await _geardownService.EndMatchAsync(args.Timeline);
+            try
+            {
+                await _geardownService.EndMatchAsync(args.Timeline);
+            }
+            catch (Exception)
+            {
+                await _server.ErrorMessageAsync(
+                    "Failed to send match results. Take screenshots and contact a match admin.");
+                throw;
+            }
         }
     }
 
     [Subscribe(ModeScriptEvent.WarmUpStart)]
-    public Task OnWarmupStart(object sender, EventArgs args) => _geardownService.FinishServerSetupAsync();
+    public async Task OnWarmupStart(object sender, EventArgs args)
+    {
+        try
+        {
+            await _geardownService.FinishServerSetupAsync();
+        }
+        catch (Exception)
+        {
+            await _server.ErrorMessageAsync("Failed to finish match setup. Contact a match admin immediately.");
+            throw;
+        }
+    }
 }
