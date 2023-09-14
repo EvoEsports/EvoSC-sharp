a7a0ee4ce240d291a13d368f4cbbdc6d98a4cd83
snixtho <snixtho@gmail.com>
2023-09-14 21:10:20 +0200

    use player nickname when sending results instead of ubisoft name

--- src/Modules/GeardownModule/Services/GeardownService.cs
+++ src/Modules/GeardownModule/Services/GeardownService.cs
@@ -110,7 +110,7 @@ public class GeardownService : IGeardownService
         await _geardownApi.Matches.AddResultsAsync((int)matchState.Match.id,
         results.Players.Select(r => new GdResult
         {
-            nickname = r.Player.UbisoftName, 
+            nickname = r.Player.NickName, 
             score = r.MatchPoints
         }));
 
