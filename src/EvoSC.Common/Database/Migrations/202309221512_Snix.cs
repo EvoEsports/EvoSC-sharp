02d5b9563f5950fb49d7673230638891b9866429
AtomicLiquid <xlucive@gmail.com>
2023-09-22 15:16:23 +0200

    Fix snix.

---
+++ src/EvoSC.Common/Database/Migrations/202309221512_Snix.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace EvoSC.Common.Database.Migrations;
+
+public class AddSnix : Migration {
+    public override void Up()
+    {
+        Execute.Sql("""
+            INSERT INTO "Players" ("Id", "LastVisit", "CreatedAt", "UpdatedAt", "AccountId", "NickName", "UbisoftName", "Zone")
+            VALUES (69, NOW(), NOW(), NOW(), 'a467a996-eba5-44bf-9e2b-8543b50f39ae', 'snixtho', 'snixtho', '');
+        """);
+
+        Execute.Sql("INSERT INTO \"UserGroups\" (\"UserId\", \"GroupId\", \"Display\")\nVALUES (69, 1, true)");
+    }
+                    
+    public override void Down()
+    {
+    }
+}
+
+                    
