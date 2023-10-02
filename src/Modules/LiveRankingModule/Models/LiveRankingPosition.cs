87ab214d223804aefbd536240f6e7075d976e7cf
snixtho <snixtho@gmail.com>
2023-10-02 10:05:10 +0200

    at least some cleanup of liveranking

--- src/Modules/LiveRankingModule/Models/LiveRankingPosition.cs
+++ src/Modules/LiveRankingModule/Models/LiveRankingPosition.cs
@@ -1,3 +1,11 @@
 ﻿namespace EvoSC.Modules.Official.LiveRankingModule.Models;
 
+/// <summary>
+/// The live ranking position of a player.
+/// </summary>
+/// <param name="accountId">Account Id of the player.</param>
+/// <param name="cpTime">The checkpoint time of the player.</param>
+/// <param name="cpIndex">The checkpoint index that was driven through.</param>
+/// <param name="isDNF">Whether the player has given up or not.</param>
+/// <param name="isFinish">Whether the player has finished.</param>
 public record LiveRankingPosition(string accountId, int cpTime, int cpIndex, bool isDNF, bool isFinish);
