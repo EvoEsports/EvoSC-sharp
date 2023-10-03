ca995d81a7768b7e44bc7012def756b1edf72eef
snixtho <snixtho@gmail.com>
2023-10-03 11:10:48 +0200

    Use form action for editing motd.

---
+++ src/Modules/MotdModule/Models/EditMotdEntryModel.cs
@@ -0,0 +1,9 @@
+﻿using EvoSC.Manialinks.Attributes;
+
+namespace EvoSC.Modules.Official.MotdModule.Models;
+
+[FormEntryModel]
+public class EditMotdEntryModel
+{
+    public string Text { get; set; }
+}
--- src/Modules/MotdModule/MotdPermissions.cs
+++ src/Modules/MotdModule/MotdPermissions.cs
@@ -8,10 +8,13 @@ public enum MotdPermissions
 {
     [Description("Set Url to fetch Motd from.")]
     SetUrl,
+    
     [Description("Interval of the timer to fetch Motd.")]
     SetFetchInterval,
+    
     [Description("Changes motd to be fetched from local.")]
     SetLocal,
+    
     [Description("Opens the Motd editor for local motd.")]
-    OpenMotdEdit
+    EditMotd
 }
