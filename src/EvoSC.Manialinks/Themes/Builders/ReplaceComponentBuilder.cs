a4fe60a65f15379937a8dc3af7bfe1363ba9f09f
snixtho <snixtho@gmail.com>
2023-11-16 09:46:34 +0100

    add theme builder tests

--- src/EvoSC.Manialinks/Themes/Builders/ReplaceComponentBuilder.cs
+++ src/EvoSC.Manialinks/Themes/Builders/ReplaceComponentBuilder.cs
@@ -9,7 +9,7 @@ where TTheme : Theme<TTheme>
     private readonly Dictionary<string, string> _componentReplacements;
     private readonly TTheme _theme;
     
-    public ReplaceComponentBuilder(string component, Dictionary<string, string> componentReplacements, TTheme theme)
+    internal ReplaceComponentBuilder(string component, Dictionary<string, string> componentReplacements, TTheme theme)
     {
         _component = component;
         _componentReplacements = componentReplacements;
---
+++ tests/EvoSC.Manialinks.Tests/Themes/ThemeBuilderTests.cs
@@ -0,0 +1,49 @@
+﻿using EvoSC.Manialinks.Themes;
+using EvoSC.Manialinks.Themes.Builders;
+
+namespace EvoSC.Manialinks.Tests.Themes;
+
+public class ThemeBuilderTests
+{
+    public class ThemeSetOption : Theme<ThemeSetOption>
+    {
+        public override Task ConfigureAsync()
+        {
+            Set("MyOption").To("My Value");
+
+            return Task.CompletedTask;
+        }
+    }
+    
+    public class ThemeReplaceComponent : Theme<ThemeReplaceComponent>
+    {
+        public override Task ConfigureAsync()
+        {
+            Replace("MyComponent").With("OtherComponent");
+
+            return Task.CompletedTask;
+        }
+    }
+    
+    [Fact]
+    public async Task Theme_Option_Is_Added()
+    {
+        var theme = new ThemeSetOption();
+
+        await theme.ConfigureAsync();
+        
+        Assert.True(theme.ThemeOptions.ContainsKey("MyOption"));
+        Assert.Equal("My Value", theme.ThemeOptions["MyOption"]);
+    }
+
+    [Fact]
+    public async Task Component_Replacement_Is_Added()
+    {
+        var theme = new ThemeReplaceComponent();
+
+        await theme.ConfigureAsync();
+        
+        Assert.True(theme.ComponentReplacements.ContainsKey("MyComponent"));
+        Assert.Equal("OtherComponent", theme.ComponentReplacements["MyComponent"]);
+    }
+}
