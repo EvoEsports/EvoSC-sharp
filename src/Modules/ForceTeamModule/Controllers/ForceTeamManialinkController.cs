f39aa8d54dc9d11943e7262943876d65a0f63e8e
snixtho <snixtho@gmail.com>
2024-07-25 10:52:27 +0200

    add tests

--- src/Modules/ForceTeamModule/Controllers/ForceTeamManialinkController.cs
+++ src/Modules/ForceTeamModule/Controllers/ForceTeamManialinkController.cs
@@ -1,15 +1,18 @@
 ﻿using EvoSC.Common.Controllers.Attributes;
-using EvoSC.Common.Interfaces.Models;
 using EvoSC.Common.Interfaces.Services;
 using EvoSC.Common.Util.TextFormatting;
 using EvoSC.Manialinks;
+using EvoSC.Manialinks.Attributes;
+using EvoSC.Modules.Official.ForceTeamModule.Events;
 using EvoSC.Modules.Official.ForceTeamModule.Interfaces;
+using EvoSC.Modules.Official.ForceTeamModule.Permissions;
 
 namespace EvoSC.Modules.Official.ForceTeamModule.Controllers;
 
 [Controller]
 public class ForceTeamManialinkController(IForceTeamService forceTeamService, IPlayerManagerService playerService) : ManialinkController
 {
+    [ManialinkRoute(Permission = ForceTeamPermissions.ForcePlayerTeam)]
     public async Task SwitchPlayerAsync(string accountId)
     {
         var player = await playerService.GetOnlinePlayerAsync(accountId);
@@ -21,8 +24,8 @@ public class ForceTeamManialinkController(IForceTeamService forceTeamService, IP
             return;
         }
 
-        var newteam = await forceTeamService.SwitchPlayerAsync(player);
-        var teamInfo = await Context.Server.Remote.GetTeamInfoAsync((int)newteam+1);
+        var newTeam = await forceTeamService.SwitchPlayerAsync(player);
+        var teamInfo = await Context.Server.Remote.GetTeamInfoAsync((int)newTeam + 1);
         
         await Context.Server.InfoMessageAsync(new TextFormatter()
             .AddText(player.NickName)
@@ -30,6 +33,11 @@ public class ForceTeamManialinkController(IForceTeamService forceTeamService, IP
             .AddText(teamInfo.Name, s => s.WithColor(teamInfo.RGB))
             .ToString());
 
+        Context.AuditEvent
+            .WithEventName(AuditEvents.PlayerSwitched)
+            .HavingProperties(new { player })
+            .Success();
+        
         await forceTeamService.ShowWindowAsync(Context.Player);
     }
 }
---
+++ src/Modules/ForceTeamModule/Events/AuditEvents.cs
@@ -0,0 +1,12 @@
+﻿using EvoSC.Common.Util.EnumIdentifier;
+
+namespace EvoSC.Modules.Official.ForceTeamModule.Events;
+
+public enum AuditEvents
+{
+    /// <summary>
+    /// Triggered when a player tries to confirm map deletion.
+    /// </summary>
+    [Identifier(Name = "ForceTeam:PlayerSwitched")]
+    PlayerSwitched,
+}
