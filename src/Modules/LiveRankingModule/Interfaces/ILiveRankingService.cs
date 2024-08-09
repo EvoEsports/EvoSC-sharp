5d007985993baf1e11a92cb5f96c46089f29dd88
Alex 'Braker' R. <araszka94@gmail.com>
2024-08-09 21:51:23 +0200

    Add LiveRankingService doc comments

--- src/Modules/LiveRankingModule/Interfaces/ILiveRankingService.cs
+++ src/Modules/LiveRankingModule/Interfaces/ILiveRankingService.cs
@@ -6,17 +6,48 @@ namespace EvoSC.Modules.Official.LiveRankingModule.Interfaces;
 
 public interface ILiveRankingService
 {
+    /// <summary>
+    /// Determines if current mode is points based and requests scores.
+    /// </summary>
+    /// <returns></returns>
     public Task DetectModeAndRequestScoreAsync();
 
+    /// <summary>
+    /// Requests scores from the game mode.
+    /// </summary>
+    /// <returns></returns>
     public Task RequestScoresAsync();
 
+    /// <summary>
+    /// Maps the given scores and displays the widget.
+    /// </summary>
+    /// <param name="scores"></param>
+    /// <returns></returns>
     public Task MapScoresAndSendWidgetAsync(ScoresEventArgs scores);
 
+    /// <summary>
+    /// Hides the live ranking widget for everyone.
+    /// </summary>
+    /// <returns></returns>
     public Task HideWidgetAsync();
 
+    /// <summary>
+    /// Determines whether a score should be displayed in the widget.
+    /// </summary>
+    /// <param name="score"></param>
+    /// <returns></returns>
     public Task<bool> ScoreShouldBeDisplayedAsync(PlayerScore score);
 
+    /// <summary>
+    /// Returns whether the current mode is points based.
+    /// </summary>
+    /// <returns></returns>
     public Task<bool> CurrentModeIsPointsBasedAsync();
 
+    /// <summary>
+    /// Converts a PlayerScore to a LiveRankingPosition object.
+    /// </summary>
+    /// <param name="score"></param>
+    /// <returns></returns>
     public Task<LiveRankingPosition> PlayerScoreToLiveRankingPositionAsync(PlayerScore score);
 }
