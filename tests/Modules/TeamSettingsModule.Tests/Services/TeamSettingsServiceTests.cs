a9e6b4393ea8bd36f46127b826590363696e7ec2
Alex 'Braker' R. <araszka94@gmail.com>
2024-08-16 20:50:45 +0200

    Fix test

--- tests/Modules/TeamSettingsModule.Tests/Services/TeamSettingsServiceTests.cs
+++ tests/Modules/TeamSettingsModule.Tests/Services/TeamSettingsServiceTests.cs
@@ -118,7 +118,6 @@ public class TeamSettingsServiceTests
         );
 
         _server.Remote.Verify(m => m.SetForcedClubLinksAsync(clubLinkUrlTeam1, clubLinkUrlTeam2), Times.Once);
-        _events.Verify(m => m.RaiseAsync(TeamSettingsEvents.SettingsUpdated, It.IsAny<TeamSettingsEventArgs>()));
     }
 
     [Fact]
