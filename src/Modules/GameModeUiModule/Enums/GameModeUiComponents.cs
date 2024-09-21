87a6685f408b5f0eea196714ba77a6c3ccc03767
Alex 'Braker' R. <araszka94@gmail.com>
2024-09-21 16:30:42 +0200

    Convert constants to static readonly properties

--- src/Modules/GameModeUiModule/Enums/GameModeUiComponents.cs
+++ src/Modules/GameModeUiModule/Enums/GameModeUiComponents.cs
@@ -2,19 +2,19 @@
 
 public static class GameModeUiComponents
 {
-    public const string Chrono = "Race_Chrono";
-    public const string RespawnHelper = "Race_RespawnHelper";
-    public const string Checkpoint = "Race_Checkpoint";
-    public const string LapsCounter = "Race_LapsCounter";
-    public const string TimeGap = "Race_TimeGap";
-    public const string ScoresTable = "Race_ScoresTable";
-    public const string DisplayMessage = "Race_DisplayMessage";
-    public const string Countdown = "Race_Countdown";
-    public const string SpectatorBaseName = "Race_SpectatorBase_Name";
-    public const string SpectatorBaseCommands = "Race_SpectatorBase_Commands";
-    public const string Record = "Race_Record";
-    public const string BigMessage = "Race_BigMessage";
-    public const string BlockHelper = "Race_BlockHelper";
-    public const string WarmUp = "Race_WarmUp";
-    public const string BestRaceViewer = "Race_BestRaceViewer";
+    public static readonly string Chrono = "Race_Chrono";
+    public static readonly string RespawnHelper = "Race_RespawnHelper";
+    public static readonly string Checkpoint = "Race_Checkpoint";
+    public static readonly string LapsCounter = "Race_LapsCounter";
+    public static readonly string TimeGap = "Race_TimeGap";
+    public static readonly string ScoresTable = "Race_ScoresTable";
+    public static readonly string DisplayMessage = "Race_DisplayMessage";
+    public static readonly string Countdown = "Race_Countdown";
+    public static readonly string SpectatorBaseName = "Race_SpectatorBase_Name";
+    public static readonly string SpectatorBaseCommands = "Race_SpectatorBase_Commands";
+    public static readonly string Record = "Race_Record";
+    public static readonly string BigMessage = "Race_BigMessage";
+    public static readonly string BlockHelper = "Race_BlockHelper";
+    public static readonly string WarmUp = "Race_WarmUp";
+    public static readonly string BestRaceViewer = "Race_BestRaceViewer";
 }
