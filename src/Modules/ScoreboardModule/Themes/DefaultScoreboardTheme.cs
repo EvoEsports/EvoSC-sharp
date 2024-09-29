751ca504a3bf8f4bbd1d3eb624c995bc3bf18638
Alex 'Braker' R. <araszka94@gmail.com>
2024-09-29 14:41:57 +0200

    Add default logo to theme

--- src/Modules/ScoreboardModule/Themes/DefaultScoreboardTheme.cs
+++ src/Modules/ScoreboardModule/Themes/DefaultScoreboardTheme.cs
@@ -14,32 +14,32 @@ public class DefaultScoreboardTheme(IThemeManager theme) : Theme<DefaultScoreboa
     {
         Set("ScoreboardModule.Text_Color").To(_theme.UI_TextPrimary);
 
-        Set("ScoreboardModule.Logo_URL").To("");
-        Set("ScoreboardModule.Logo_Width").To(10.0);
-        Set("ScoreboardModule.Logo_Height").To(10.0);
+        Set("ScoreboardModule.Logo_URL").To("file://Media/Manialinks/Nadeo/Trackmania/Menus/TMLogo.dds");
+        Set("ScoreboardModule.Logo_Width").To("20.0");
+        Set("ScoreboardModule.Logo_Height").To("10.0");
         
-        Set("ScoreboardModule.Background_Opacity").To(0.0);
+        Set("ScoreboardModule.Background_Opacity").To("0.0");
         Set("ScoreboardModule.Background_Image").To("");
         
         Set("ScoreboardModule.Background_Header_Color").To(_theme.UI_HeaderBg);
-        Set("ScoreboardModule.Background_Header_Opacity").To(0.95);
+        Set("ScoreboardModule.Background_Header_Opacity").To("0.95");
         
         Set("ScoreboardModule.Background_Legend_Color").To(_theme.UI_HeaderBg);
-        Set("ScoreboardModule.Background_Legend_Opacity").To(1.0);
+        Set("ScoreboardModule.Background_Legend_Opacity").To("1.0");
         Set("ScoreboardModule.Background_Legend_Text_Color").To(_theme.UI_TextPrimary);
-        Set("ScoreboardModule.Background_Legend_Text_Opacity").To(0.75);
+        Set("ScoreboardModule.Background_Legend_Text_Opacity").To("0.75");
         
         Set("ScoreboardModule.Background_Row_Color").To(_theme.UI_BgPrimary);
-        Set("ScoreboardModule.Background_Row_Opacity").To(0.9);
+        Set("ScoreboardModule.Background_Row_Opacity").To("0.9");
         
         Set("ScoreboardModule.Background_Hover_Color").To(_theme.UI_BgHighlight);
-        Set("ScoreboardModule.Background_Hover_Opacity").To(0.9);
+        Set("ScoreboardModule.Background_Hover_Opacity").To("0.9");
         
         Set("ScoreboardModule.PositionBox_ShowAccent").To("True");
         Set("ScoreboardModule.PositionBox_Color").To(_theme.UI_AccentSecondary);
-        Set("ScoreboardModule.PositionBox_Opacity").To(1.0);
+        Set("ScoreboardModule.PositionBox_Opacity").To("1.0");
         Set("ScoreboardModule.PositionBox_TextColor").To(_theme.UI_TextSecondary);
-        Set("ScoreboardModule.PositionBox_TextOpacity").To(1.0);
+        Set("ScoreboardModule.PositionBox_TextOpacity").To("1.0");
 
         Set("ScoreboardModule.Background_Row_Flag_AlphaMaskUrl").To("file://Media/Manialinks/Nadeo/Trackmania/Menus/Common/Common_Flag_Mask.dds");
         
