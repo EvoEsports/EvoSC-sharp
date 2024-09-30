0d508c79995ee1287882421c8f9e4d67fdb37a03
Alex 'Braker' R. <araszka94@gmail.com>
2024-09-30 20:49:20 +0200

    Add warmup round mode script events

---
+++ src/EvoSC.Common/Remote/EventArgsModels/WarmUpRoundEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace EvoSC.Common.Remote.EventArgsModels;
+
+public class WarmUpRoundEventArgs: EventArgs
+{
+    /// <summary>
+    /// The current warm-up round.
+    /// </summary>
+    public required int Current { get; init; }
+
+    /// <summary>
+    /// The total amount of warm-ups.
+    /// </summary>
+    public required int Total { get; init; }
+}
