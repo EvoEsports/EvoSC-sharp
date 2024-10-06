80fb3d69d6b5c32d590bdd001e8acb3488a0c954
Alex 'Braker' R. <araszka94@gmail.com>
2024-10-06 11:21:11 +0200

    Add round rnaking tests project

--- src/Modules/RoundRankingModule/Models/CheckpointsRepository.cs
+++ src/Modules/RoundRankingModule/Models/CheckpointsRepository.cs
@@ -5,8 +5,7 @@ public class CheckpointsRepository : Dictionary<string, CheckpointData>
     public List<CheckpointData> GetSortedData()
     {
         return this.Values
-            .OrderBy(cpData => cpData.IsDNF)
-            .ThenByDescending(cpData => cpData.CheckpointId)
+            .OrderByDescending(cpData => cpData.CheckpointId)
             .ThenBy(cpData => cpData.Time.TotalMilliseconds)
             .ToList();
     }
---
+++ tests/Modules/RoundRankingModule.Tests/Controllers/RoundRankingEventControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using EvoSC.Common.Interfaces.Controllers;
+using EvoSC.Common.Remote.EventArgsModels;
+using EvoSC.Modules.Official.RoundRankingModule.Controllers;
+using EvoSC.Modules.Official.RoundRankingModule.Interfaces;
+using EvoSC.Testing.Controllers;
+using Moq;
+using Xunit;
+
+namespace EvoSC.Modules.Official.RoundRankingModule.Tests.Controllers;
+
+public class RoundRankingEventControllerTests : ControllerMock<RoundRankingEventController, IEventControllerContext>
+{
+    private Mock<IRoundRankingService> _roundRankingService = new();
+
+    public RoundRankingEventControllerTests()
+    {
+        InitMock(_roundRankingService);
+    }
+
+    [Fact]
+    public async Task Consumes_Checkpoint_Data()
+    {
+        Controller.OnWaypointAsync(null,
+            new WayPointEventArgs
+            {
+                Login = "*fakeplayer1*",
+                AccountId = "*fakeplayer1*",
+                RaceTime = 0,
+                LapTime = 0,
+                CheckpointInRace = 0,
+                CheckpointInLap = 0,
+                IsEndRace = false,
+                IsEndLap = false,
+                CurrentRaceCheckpoints = [],
+                CurrentLapCheckpoints = [],
+                BlockId = "",
+                Speed = 0,
+                Time = 0
+            });
+    }
+}
