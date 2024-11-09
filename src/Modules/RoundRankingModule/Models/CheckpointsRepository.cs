0aebb7075900693008ccfd38373704e15cebaaf7
Alex 'Braker' R. <araszka94@gmail.com>
2024-11-09 14:52:59 +0100

    Add CheckpointsRepository doc comments

--- src/Modules/RoundRankingModule/Models/CheckpointsRepository.cs
+++ src/Modules/RoundRankingModule/Models/CheckpointsRepository.cs
@@ -2,6 +2,10 @@
 
 namespace EvoSC.Modules.Official.RoundRankingModule.Models;
 
+/// <summary>
+/// CheckpointsRepository contains all <seealso cref="CheckpointData"/> for the ongoing round,
+/// where the key of the dictionary is the players account-ID.
+/// </summary>
 public class CheckpointsRepository : ConcurrentDictionary<string, CheckpointData>
 {
     public List<CheckpointData> GetSortedData()
