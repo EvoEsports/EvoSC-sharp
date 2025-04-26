fdd6244dff3d4ede0917dc4a2c8ade18169c6bdd
Alex 'Braker' R. <araszka94@gmail.com>
2025-04-26 13:39:33 +0200

    Remove IToggleable interface from MatchManagerModule

--- src/Modules/MatchManagerModule/MatchManagerModule.cs
+++ src/Modules/MatchManagerModule/MatchManagerModule.cs
@@ -1,15 +1,8 @@
 ﻿using EvoSC.Modules.Attributes;
-using EvoSC.Modules.Interfaces;
-using EvoSC.Modules.Official.MatchManagerModule.Interfaces;
 
 namespace EvoSC.Modules.Official.MatchManagerModule;
 
 [Module(IsInternal = true)]
-public class MatchManagerModule(IMatchControlService matchControlService) : EvoScModule, IToggleable
+public class MatchManagerModule : EvoScModule
 {
-    public Task EnableAsync()
-        => matchControlService.RequestScoresAsync();
-
-    public Task DisableAsync()
-        => Task.CompletedTask;
 }
