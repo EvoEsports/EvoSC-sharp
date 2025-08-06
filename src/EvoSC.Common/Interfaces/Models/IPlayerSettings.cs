2d759ef6ca6183f6ef0071a1d51378b46d80594b
Alex 'Braker' R. <araszka94@gmail.com>
2025-08-06 20:01:52 +0200

    Change interface to require IEnumerable instead of List

--- src/EvoSC.Common/Interfaces/Models/IPlayerSettings.cs
+++ src/EvoSC.Common/Interfaces/Models/IPlayerSettings.cs
@@ -6,7 +6,7 @@ public interface IPlayerSettings
 
     public string? HiddenManialinks { get; set; }
 
-    public List<string> GetHiddenManialinks();
+    public IEnumerable<string> GetHiddenManialinks();
 
     public void SetHiddenManialinks(List<string> hiddenManialinks);
 }
