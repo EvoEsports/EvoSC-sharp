d5a7f39f901439237fecfe7b2d04b62cea872fe2
snixtho <snixtho@gmail.com>
2025-08-30 19:04:09 +0200

    Implement widget updates and fix player comparisons.

--- src/Modules/MatchReadyModule/Services/ReadyManialinkService.cs
+++ src/Modules/MatchReadyModule/Services/ReadyManialinkService.cs
@@ -4,11 +4,12 @@ using EvoSC.Common.Services.Attributes;
 using EvoSC.Common.Services.Models;
 using EvoSC.Manialinks.Interfaces;
 using EvoSC.Modules.Official.MatchReadyModule.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace EvoSC.Modules.Official.MatchReadyModule.Services;
 
 [Service(LifeStyle = ServiceLifeStyle.Transient)]
-public class ReadyManialinkService(IReadyService readyService, IManialinkManager manialinks, IPlayerManagerService playerManager) : IReadyManialinkService
+public class ReadyManialinkService(IReadyService readyService, IManialinkManager manialinks, IPlayerManagerService playerManager, ILogger<ReadyManialinkService> logger) : IReadyManialinkService
 {
     public async Task SendWidgetAsync()
     {
@@ -20,8 +21,10 @@ public class ReadyManialinkService(IReadyService readyService, IManialinkManager
         
         foreach (var player in allPlayers)
         {
-            var isReady = readyService.ReadyPlayers.Contains(player);
-            var showButton = readyService.Players.Contains(player);
+            var isReady = readyService.ReadyPlayers.Any(p => p.AccountId == player.AccountId);
+            var showButton = readyService.Players.Any(p => p.AccountId == player.AccountId);
+            
+            logger.LogDebug("Showing ReadyWidget for player (isReady: {IsReady}, showButton: {ShowButton})", isReady, showButton);
 
             await trans.SendManialinkAsync(player, "MatchReadyModule.ReadyWidget", new
             {
@@ -37,12 +40,30 @@ public class ReadyManialinkService(IReadyService readyService, IManialinkManager
         var requiredPlayers = readyService.Players.Count();
         var playersReady = readyService.ReadyPlayers.Count();
         
-        var isReady = readyService.ReadyPlayers.Contains(player);
-        var showButton = readyService.Players.Contains(player);
+        var isReady = readyService.ReadyPlayers.Any(p => p.AccountId == player.AccountId);
+        var showButton = readyService.Players.Any(p => p.AccountId == player.AccountId);
 
         await manialinks.SendManialinkAsync(player, "MatchReadyModule.ReadyWidget", new
         {
             isReady, requiredPlayers, playersReady, showButton
         });
+    }
+
+    public async Task UpdateWidgetAsync()
+    {
+        var readyPlayersCount = readyService.Players.Count();
+        var allPlayers = await playerManager.GetOnlinePlayersAsync();
+        
+        var trans = manialinks.CreateTransaction();
+
+        foreach (var player in allPlayers)
+        {
+            var isReady = readyService.ReadyPlayers.Any(p => p.AccountId == player.AccountId);
+
+            logger.LogDebug("Updating ReadyWidget for player (isReady: {IsReady})", isReady);
+            await trans.SendManialinkAsync(player, "MatchReadyModule.UpdateWidget", new { readyPlayersCount, isReady });
+        }
+        
+        await trans.CommitAsync();
     }
 }
