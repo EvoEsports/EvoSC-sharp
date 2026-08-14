13939174dd2d443aff2c57ae2637eb91078b2359
Christopher Flügel <christopher.fluegel@gmail.com>
2026-08-15 01:29:35 +0200

    Refactor ASayController and tests: update ICommandInteractionContext usage and improve mock initialization
    (cherry picked from commit 0c5758e1a10f8991f6efbd946621c2a4117ace56)

--- src/Modules/ASayModule/Controllers/ASayController.cs
+++ src/Modules/ASayModule/Controllers/ASayController.cs
@@ -1,5 +1,5 @@
-﻿using EvoSC.Commands;
-using EvoSC.Commands.Attributes;
+﻿using EvoSC.Commands.Attributes;
+using EvoSC.Commands.Interfaces;
 using EvoSC.Common.Controllers;
 using EvoSC.Common.Controllers.Attributes;
 using EvoSC.Modules.Official.ASayModule.Events;
@@ -8,7 +8,7 @@ using EvoSC.Modules.Official.ASayModule.Interfaces;
 namespace EvoSC.Modules.Official.ASayModule.Controllers;
 
 [Controller]
-public class ASayController(IASayService asayService) : EvoScController<CommandInteractionContext>
+public class ASayController(IASayService asayService) : EvoScController<ICommandInteractionContext>
 {
     [ChatCommand("asay", "Shows a message to all connected players as manialink.", ASayPermissions.UseASay, true)]
     public async Task ShowAnnounceMessageToPlayersAsync(string? text)
