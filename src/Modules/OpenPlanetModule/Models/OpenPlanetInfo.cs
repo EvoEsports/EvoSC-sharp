f5002008aebb0430b229e0a943058603a1219426
Christopher Flügel <christopher.fluegel@gmail.com>
2026-08-28 20:32:59 +0200

    Update localization and UI positioning in OpenPlanet module
    (cherry picked from commit 573031e4d3ca90018761b9882407675ff04b34af)

--- src/Modules/OpenPlanetModule/Models/OpenPlanetInfo.cs
+++ src/Modules/OpenPlanetModule/Models/OpenPlanetInfo.cs
@@ -7,7 +7,7 @@ namespace EvoSC.Modules.Official.OpenPlanetModule.Models;
 public class OpenPlanetInfo : IOpenPlanetInfo
 {
     private static readonly Regex OpenPlanetRegex =
-        new("^Openplanet ([\\d.]+) \\((\\w+), ([A-Z]\\w+), (\\d{4}-\\d{2}-\\d{2})\\)(?:\\s(?:\\[([A-Z]+)\\]))*$");
+        new("^Openplanet ([\\d.]+) \\((\\w+), ([A-Z]\\w+), (\\w+)\\)(?:\\s\\[([A-Z]+)\\])?$");
     
     public Version Version { get; set; }
     public string Game { get; set; }
